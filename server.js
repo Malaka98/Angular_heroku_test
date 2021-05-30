9164e35969741b3d9f37c61c8ad789a7d1fd09fe
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-30 14:34:36 +0530

    Fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('./dist/Angular_heroku_test'));
 
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', {root: 'dist/Angular_heroku_test'});
+  res.sendFile('index.html', {root: 'dist/Angular_heroku_test/'});
 });
 
 app.listen(5000 || process.env.PORT, () => {
