c298f2562f6b6b7b54ff72457e77b92faec59cb0
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-30 13:42:37 +0530

    Update express server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'dist/my-app', 'index.html'));
