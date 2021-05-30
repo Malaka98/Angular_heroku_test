b6f74c1120c79613cff530460d6a3e2338fbe5f9
Malaka98 <malaka.aruna14413@gmail.com>
2021-05-30 14:12:05 +0530

    Fix

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist/my-app', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-app.listen(9000 || process.env.PORT, () => {
+app.listen(5000 || process.env.PORT, () => {
   console.log("Server start");
 });
